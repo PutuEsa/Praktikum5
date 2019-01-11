b59acbe83ba41f3f218f4ae33a0784066f375a77
PutuEsa <46438115+PutuEsa@users.noreply.github.com>
2019-01-11 19:56:04 +0700

    Delete tanpareturn.php

--- tanpareturn.php
+++
@@ -1,8 +0,0 @@
-<?php
-function head() 
-{ echo'
-<link rel="stylesheet" type="text/css" href="style/style.css">
-<link rel="stylesheet" type="text/css" href="style/font-awesome.css">
-';}
-echo luas_lingkaran($r);
-?>
